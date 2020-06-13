e7657c8ccfd2f2d9c3ff98655b1933c35f715e23
Augusto Emiliano Protillo <aportillo@desarrollosnea.com>
2020-06-13 16:10:52 -0300

    Navigate

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Listado from "./views/listadoProductos/listado";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+// const App = StackNavigator({
+//   // Home: { screen: HomeScreen },
+//   //La llave del objeto es el identificador del screen y lo vas a usar en el navigation.
+//   Listado: { screen: Listado },
+// });
 
-export default function App() {
+// export default function App() {
+//   const { navigate } = this.props.navigation;
+//   return (
+//     <View style={styles.container} onTouchStart={() => navigate(Listado)}>
+//       <Image source={require("./assets/logo.png")} />
+//     </View>
+//   );
+// }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
+    <View
+      style={styles.container}
+      onTouchEnd={() => navigation.push("Listado")}
+    >
       <Image source={require("./assets/logo.png")} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Listado" component={Listado} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ views/listadoProductos/listado.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+export default function Listado() {
+  return (
+    <View style={styles.container}>
+      <Button>Botoncito de otra vista</Button>
+    </View>
+  );
+}
