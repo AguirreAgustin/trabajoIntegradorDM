cd558e48bcedb7f72b2706b789600a8940e12024
Augusto Emiliano Protillo <aportillo@desarrollosnea.com>
2020-06-13 16:44:29 -0300

    add Navigate

--- views/listadoProductos/listado.js
+++ views/listadoProductos/listado.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default function Listado() {
   return (
     <View style={styles.container}>
-      <Button>Botoncito de otra vista</Button>
+      <Button title="Botoncito para listado" />
     </View>
   );
 }
