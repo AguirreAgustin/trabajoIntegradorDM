752e9242ef9923b36dddd6ccff1eea3c8b5c147c
Augusto Emiliano Protillo <aportillo@desarrollosnea.com>
2020-06-18 23:49:06 -0300

    Splah Inicial

--- App/Navigations/navigations.js
+++ App/Navigations/navigations.js
@@ -1,26 +1,62 @@
 import React from "react";
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator} from '@react-navigation/stack';
-import MenuHamburguesa from './menuHamburguesa'
-import ContentDrawer from './contentDrawer'
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import MenuHamburguesa from "./menuHamburguesa";
+import ContentDrawer from "./contentDrawer";
 
-import Home from '../Views/Home'
+import Home from "../Views/Home";
 import Listado from "../Views/listado";
-
+import PageInit from "../Views/PageInit";
+import PageInit2 from "../Views/PageInit2";
+import PageInit3 from "../Views/PageInit3";
 
 const Stack = createStackNavigator();
 const FlujoCompradorStack = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen
+        name="PageInit"
+        component={PageInit}
+        options={({ navigation }) => ({
+          headerTitleAlign: "center",
+          headerTintColor: "black",
+          headerLeft: (props) => (
+            <MenuHamburguesa {...props} navigation={navigation} />
+          ),
+        })}
+      />
+      <Stack.Screen
+        name="PageInit2"
+        component={PageInit2}
+        options={({ navigation }) => ({
+          headerTitleAlign: "center",
+          headerTintColor: "black",
+          headerLeft: (props) => (
+            <MenuHamburguesa {...props} navigation={navigation} />
+          ),
+        })}
+      />
+      <Stack.Screen
+        name="PageInit3"
+        component={PageInit3}
+        options={({ navigation }) => ({
+          headerTitleAlign: "center",
+          headerTintColor: "black",
+          headerLeft: (props) => (
+            <MenuHamburguesa {...props} navigation={navigation} />
+          ),
+        })}
+      />
+      <Stack.Screen
         name="Home"
         component={Home}
         options={({ navigation }) => ({
           headerTitleAlign: "center",
-          headerTintColor: 'black',
-          headerLeft: props => (<MenuHamburguesa {...props} navigation={navigation} />),
-
+          headerTintColor: "black",
+          headerLeft: (props) => (
+            <MenuHamburguesa {...props} navigation={navigation} />
+          ),
         })}
       />
       <Stack.Screen
@@ -28,33 +64,29 @@ const FlujoCompradorStack = () => {
         component={Listado}
         options={({ navigation }) => ({
           headerTitleAlign: "center",
-          headerTintColor: 'black',
-          headerLeft: props => (<MenuHamburguesa {...props} navigation={navigation} />),
-
+          headerTintColor: "black",
+          headerLeft: (props) => (
+            <MenuHamburguesa {...props} navigation={navigation} />
+          ),
         })}
       />
     </Stack.Navigator>
   );
-}
-
+};
 
 const Drawer = createDrawerNavigator();
 const DStack = () => {
   return (
-    <Drawer.Navigator
-      drawerContent={props => ContentDrawer(props)}
-     >
+    <Drawer.Navigator drawerContent={(props) => ContentDrawer(props)}>
       <Drawer.Screen name="FlujoComprador" component={FlujoCompradorStack} />
     </Drawer.Navigator>
   );
+};
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <DStack />
+    </NavigationContainer>
+  );
 }
-
-
-export default function Navigation(){
-    return (
-        <NavigationContainer>
-            <DStack />
-        </NavigationContainer>
-    )
-}
-
---
+++ App/Views/PageInit3.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+
+var index = 0;
+const page1 = require("../../assets/page1.jpeg");
+const page2 = require("../../assets/page2.jpeg");
+const page3 = require("../../assets/page3.jpeg");
+
+const changeView = function (navigation) {
+  navigation.push("Home");
+};
+
+export default function PageInit3({ navigation }) {
+  return (
+    <View style={styles.container} onTouchStart={() => changeView(navigation)}>
+      <Image
+        // style={{ width: "100%", height: "100%" }}
+        source={page3}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  botonCompra: { marginBottom: "10px", color: "red" },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    //justifyContent: "center",
+  },
+});
